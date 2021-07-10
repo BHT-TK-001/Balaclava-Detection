958b68e237388ecc2b57bfc9dcfdb0fd30ef7478
BHT-TK-001 <bhavinpatil56@gmail.com>
2021-07-10 14:37:32 +0530

    First commit

--- FaceMask/settings.py
+++ FaceMask/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9!o(m=a+ri2hf7ljr7)stc-%xlyqdxt20wb@=!183e8wd=f-vw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','balaclavadetector.herokuapp.com','balaclava-detection.herokuapp.com']
 
