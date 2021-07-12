e957e4d24e55f0544c71d4deec15c6f25ffef49a
BHT-TK-001 <bhavinpatil56@gmail.com>
2021-07-12 08:42:42 +0530

    First Commit

--- VISOR/face.py
+++ VISOR/face.py
@@ -9,10 +9,6 @@ import winsound
 frequency = 5000 
 duration = 100
 from django.conf import settings
-# face_detection_videocam = cv2.CascadeClassifier(os.path.join(
-# 			settings.BASE_DIR,'dataset/with_mask'))
-# face_detection_webcam = cv2.CascadeClassifier(os.path.join(
-# 			settings.BASE_DIR,'dataset/without_mask'))
 face_detection_videocam = cv2.CascadeClassifier(os.path.join(
 			settings.BASE_DIR,'dataset/with_mask'))
 face_detection_webcam = cv2.CascadeClassifier(os.path.join(
@@ -44,31 +40,6 @@ class VideoCamera(object):
 		frame_flip = cv2.flip(image,1)
 		ret, jpeg = cv2.imencode('.jpg', frame_flip)
 		return jpeg.tobytes()
-
-
-# class IPWebCam(object):
-# 	def __init__(self):
-# 		self.url = ""
-
-# 	def __del__(self):
-# 		cv2.destroyAllWindows()
-# 		self.vs.stop()
-
-# 	def get_frame(self):
-# 		imgResp = urllib.request.urlopen(self.url)
-# 		imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
-# 		img= cv2.imdecode(imgNp,-1)
-# 		# We are using Motion JPEG, but OpenCV defaults to capture raw images,
-# 		# so we must encode it into JPEG in order to correctly display the
-# 		# video stream
-# 		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# 		faces_detected = face_detection_webcam.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-# 		for (x, y, w, h) in faces_detected:
-# 			cv2.rectangle(img, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
-# 		resize = cv2.resize(img, (640, 480), interpolation = cv2.INTER_LINEAR) 
-# 		frame_flip = cv2.flip(resize,1)
-# 		ret, jpeg = cv2.imencode('.jpg', frame_flip)
-# 		return jpeg.tobytes()
 
 
 class MaskDetect(object):
@@ -176,16 +147,3 @@ class MaskDetect(object):
 		ret, jpeg = cv2.imencode('.jpg', frame)
 		return jpeg.tobytes()
 		
-# class LiveWebCam(object):
-# 	def __init__(self):
-# 		self.url = cv2.VideoCapture("")
-
-# 	def __del__(self):
-# 		cv2.destroyAllWindows()
-# 		self.vs.stop()
-
-# 	def get_frame(self):
-# 		success,imgNp = self.url.read()
-# 		resize = cv2.resize(imgNp, (640, 480), interpolation = cv2.INTER_LINEAR) 
-# 		ret, jpeg = cv2.imencode('.jpg', resize)
-# 		return jpeg.tobytes()
--- VISOR/helmet.py
+++ VISOR/helmet.py
@@ -9,11 +9,6 @@ import winsound
 frequency = 500 
 duration = 100
 from django.conf import settings
-# face_detection_videocam = cv2.CascadeClassifier(os.path.join(
-# 			settings.BASE_DIR,'dataset/with_mask'))
-# face_detection_webcam = cv2.CascadeClassifier(os.path.join(
-# 			settings.BASE_DIR,'dataset/without_mask'))
-# load our serialized face detector model from disk
 prototxtPath = os.path.sep.join([settings.BASE_DIR, "models/deploy.prototxt"])
 weightsPath = os.path.sep.join([settings.BASE_DIR,"models/res10_300x300_ssd_iter_140000.caffemodel"])
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
@@ -40,31 +35,6 @@ class VideoCamera(object):
 		frame_flip = cv2.flip(image,1)
 		ret, jpeg = cv2.imencode('.jpg', frame_flip)
 		return jpeg.tobytes()
-
-
-# class IPWebCam(object):
-# 	def __init__(self):
-# 		self.url = ""
-
-# 	def __del__(self):
-# 		cv2.destroyAllWindows()
-# 		self.vs.stop()
-
-# 	def get_frame(self):
-# 		imgResp = urllib.request.urlopen(self.url)
-# 		imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
-# 		img= cv2.imdecode(imgNp,-1)
-# 		# We are using Motion JPEG, but OpenCV defaults to capture raw images,
-# 		# so we must encode it into JPEG in order to correctly display the
-# 		# video stream
-# 		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# 		faces_detected = face_detection_webcam.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-# 		for (x, y, w, h) in faces_detected:
-# 			cv2.rectangle(img, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
-# 		resize = cv2.resize(img, (640, 480), interpolation = cv2.INTER_LINEAR) 
-# 		frame_flip = cv2.flip(resize,1)
-# 		ret, jpeg = cv2.imencode('.jpg', frame_flip)
-# 		return jpeg.tobytes()
 
 
 class HelmetDetection(object):
@@ -115,7 +85,7 @@ class HelmetDetection(object):
 				# ordering, resize it to 224x224, and preprocess it
 				face = frame[startY:endY, startX:endX]
 				face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-				face = cv2.resize(face, (76, 76))
+				face = cv2.resize(face, (224, 224))
 				face = img_to_array(face)
 				face = preprocess_input(face)
 
@@ -170,17 +140,3 @@ class HelmetDetection(object):
 			cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 		ret, jpeg = cv2.imencode('.jpg', frame)
 		return jpeg.tobytes()
-		
-# class LiveWebCam(object):
-# 	def __init__(self):
-# 		self.url = cv2.VideoCapture("")
-
-# 	def __del__(self):
-# 		cv2.destroyAllWindows()
-# 		self.vs.stop()
-
-# 	def get_frame(self):
-# 		success,imgNp = self.url.read()
-# 		resize = cv2.resize(imgNp, (640, 480), interpolation = cv2.INTER_LINEAR) 
-# 		ret, jpeg = cv2.imencode('.jpg', resize)
-# 		return jpeg.tobytes()
